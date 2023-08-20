809b266c6ecdef826027eaccaabf9dc19b8751df
amirparvaneh <amirparvaneh138@gmail.com>
2023-08-20 18:02:09 +0330

    mtn-dev-1

---
+++ src/test/java/com/mtn/financerecommendation/FinanceRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtn.financerecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
